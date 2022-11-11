01313d9bb824294e45cc21fb1ced414fc76cdb8a
Yavuz Selimhan KAYA <yavuzselimhankaya@icloud.com>
2022-11-11 17:42:00 +0300

    1.2.0
    Added "Restore" Function!

--- src/extension.ts
+++ src/extension.ts
@@ -3,32 +3,59 @@ import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('rcb.backup', (uri:vscode.Uri) => {
+	context.subscriptions.push(vscode.commands.registerCommand('right-click-backup.backup', (uri:vscode.Uri) => {
 		
 		var backupfile = uri.fsPath + '.bak';
 
 		if (fs.existsSync(backupfile)) {
 			vscode.window
-			.showInformationMessage(".bak already exist do you want to overwrite", "Yes", "No")
+			.showInformationMessage("Backup already exist do you want to overwrite", "Yes", "No")
 			.then(answer => {
 				if (answer === "Yes") {
 					fs.copyFile(uri.fsPath, backupfile, (err) => {
 						if (err) throw err;
-						console.log('File Backed Up!');
+						vscode.window.showInformationMessage('Backed Up!');
 					});
 				} else {
 					console.log("File didn't backed up")
 				}
-			})
+			});
 		} else {
 			fs.copyFile(uri.fsPath, backupfile, (err) => {
 				if (err) throw err;
 				console.log('File Backed Up!');
 			});
 		}
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('right-click-backup.restore', (uri:vscode.Uri) => {
+		
+		var restorefile = uri.fsPath.replace('.bak', '');
+
+		var fileName = restorefile.split("/");
+
+		var showInformationMessage = fileName[fileName.length-1] + ' already exist do you want to overwrite?';
+
+		if (fs.existsSync(restorefile)) {
+			vscode.window
+			.showInformationMessage(showInformationMessage, "Yes", "No")
+			.then(answer => {
+				if (answer === "Yes") {
+					fs.copyFile(uri.fsPath, restorefile, (err) => {
+						if (err) throw err;
+						vscode.window.showInformationMessage('Restored!');
+					});
+				} else {
+					console.log("User De-Activated")
+				}
+			});
+		} else {
+			fs.copyFile(uri.fsPath, restorefile, (err) => {
+				if (err) throw err;
+				console.log('File Restored!');
+			});
+		}
+	}));
 }
 
 export function deactivate() {}
